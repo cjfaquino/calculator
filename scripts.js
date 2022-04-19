c7789cd84848d08c3c5e0144fcec416ea6595f80
Christian Aquino <cjfaquino@gmail.com>
2022-04-19 03:39:03 -0700

    add operate function

--- scripts.js
+++ scripts.js
@@ -13,3 +13,10 @@ function multiply(a ,b) {
 function divide(a, b) {
   return a / b
 }
+
+function operate(operator, currentValue, nextValue) {
+  if(operator === add) return add(currentValue, nextValue);
+  if(operator === subtract) return subtract(currentValue, nextValue);
+  if(operator === multiply) return multiply(currentValue, nextValue);
+  if(operator === divide) return divide(currentValue, nextValue);
+}
