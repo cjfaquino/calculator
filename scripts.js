c18ec4787be8bce4d5ba2129059644c4708385e7
Christian Aquino <cjfaquino@gmail.com>
2022-04-21 05:31:21 -0700

    refactor to accept keypress

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 const calculator = document.querySelector('.wrapper')
 const display = document.getElementById('display');
 const buttons = document.querySelector('.buttons');
+document.addEventListener('keydown', pressKey)
+buttons.addEventListener('click', pressKey)
 
 let firstValue;
 let secondValue;
@@ -30,110 +32,117 @@ function populateDisplay(input) {
   }
 }
 
-buttons.addEventListener('click', e => {
+function pressKey(e) {
+  if(e.target.matches('button') ||
+    e.keyCode >= 48 && e.keyCode <= 57 || //number keys
+    e.keyCode === 8 ||                     //backspace
+    e.keyCode === 190 ||                   //decimal
+    e.keyCode === 189 ||                   //minus
+    e.keyCode === 187 ||                   //equals + plus
+    e.keyCode === 13 ||                    //enter
+    e.keyCode === 27)                      //escape
   {
-    if(e.target.matches('button')) {
-      const button = e.target
-      const action = e.target.value
-      Array.from(button.parentNode.children)
-          .forEach(btn => btn.classList.remove('pressed'))
-  
-      if(!action){
-        populateDisplay(button.textContent);
-        calculator.dataset.previousKeyType = 'number'
-      }
-  
-      if(action === 'add' ||
-        action === 'subtract' ||
-        action === 'multiply' ||
-        action === 'divide'){
-          
-          firstValue = storedValue;
-          operator = lastOperator;
-          secondValue = display.textContent
-  
-          if(firstValue && 
-             operator &&
-             calculator.dataset.previousKeyType !== 'operator' &&
-             calculator.dataset.previousKeyType !== 'equals') {
-              const calculatedValue = operate(operator, firstValue, secondValue);
-               display.textContent = calculatedValue;
-               storedValue = calculatedValue;
-          } else {
-            storedValue = display.textContent;
-          }
-  
-          button.classList.add('pressed')
-          calculator.dataset.previousKeyType = 'operator'
-          lastOperator = action;
-        }
-  
-      if(action === 'decimal') {
+    const button = e.target
+    const action = e.target.value
+    Array.from(button.parentNode.children)
+        .forEach(btn => btn.classList.remove('pressed'))
+
+    if(!action && button.className.includes('numbers')){
+      populateDisplay(button.textContent);
+      calculator.dataset.previousKeyType = 'number'
+    }
+
+    if(action === 'add' ||
+      action === 'subtract' ||
+      action === 'multiply' ||
+      action === 'divide'){
         
-        if(!display.textContent.includes('.')){
-          display.textContent = display.textContent + '.'
-        }
-        if (calculator.dataset.previousKeyType === 'operator' ||
-            calculator.dataset.previousKeyType === 'equals') {
-          display.textContent = '0.'
-        }
-        calculator.dataset.previousKeyType = 'decimal'
-      }
-      
-      if(action === 'negative') {
-
-        if(!isNegative && Number(display.textContent) > 0) {
-          display.textContent = '-' + display.textContent
-          isNegative = true;
-          return
-        }
-        else if(isNegative) {
-          display.textContent = display.textContent.substring(1);
-          isNegative = false;
-          return
-        }
-      }
-  
-      if(action === 'delete') {
-
-        if(display.textContent.length > 1)
-          display.textContent = display.textContent.slice(0, -1)
-        else display.textContent = 0;
-      }
-      
-      if(action !== 'clear') {
-        const clear = buttons.querySelector('.btnClr')
-        clear.textContent = 'CE'
-      }
-      
-      if(action === 'clear') {
-        if(button.textContent === 'AC') {
-          firstValue = '';
-          secondValue = '';
-          storedValue = '';
-          storedSecond = '';
-          calculator.dataset.previousKeyType = ''
-        } else button.textContent = 'AC';
-
-        display.textContent = 0;
-        calculator.dataset.previousKeyType = 'clear'
-      }
-  
-      if(action === 'equals') {
-
         firstValue = storedValue;
         operator = lastOperator;
-        secondValue = display.textContent;
-        if(firstValue) {
-          if(calculator.dataset.previousKeyType === 'equals') {
-            firstValue = display.textContent;
-            secondValue = storedSecond;
-          }
-          display.textContent = operate(operator, firstValue, secondValue);
+        secondValue = display.textContent
+
+        if(firstValue && 
+            operator &&
+            calculator.dataset.previousKeyType !== 'operator' &&
+            calculator.dataset.previousKeyType !== 'equals') {
+            const calculatedValue = operate(operator, firstValue, secondValue);
+              display.textContent = calculatedValue;
+              storedValue = calculatedValue;
+        } else {
+          storedValue = display.textContent;
         }
-        storedSecond = secondValue;
-        calculator.dataset.previousKeyType = 'equals'
+
+        button.classList.add('pressed')
+        calculator.dataset.previousKeyType = 'operator'
+        lastOperator = action;
+      }
+
+    if(action === 'decimal') {
+      
+      if(!display.textContent.includes('.')){
+        display.textContent = display.textContent + '.'
+      }
+      if (calculator.dataset.previousKeyType === 'operator' ||
+          calculator.dataset.previousKeyType === 'equals') {
+        display.textContent = '0.'
+      }
+      calculator.dataset.previousKeyType = 'decimal'
+    }
+    
+    if(action === 'negative') {
+
+      if(!isNegative && Number(display.textContent) > 0) {
+        display.textContent = '-' + display.textContent
+        isNegative = true;
+        return
+      }
+      else if(isNegative) {
+        display.textContent = display.textContent.substring(1);
+        isNegative = false;
+        return
       }
     }
+
+    if(action === 'delete'|| 
+       e.key === 'Backspace') {
+
+      if(display.textContent.length > 1)
+        display.textContent = display.textContent.slice(0, -1)
+      else display.textContent = 0;
+    }
+    
+    if(action !== 'clear') {
+      const clear = buttons.querySelector('.btnClr')
+      clear.textContent = 'CE'
+    }
+    
+    if(action === 'clear') {
+      if(button.textContent === 'AC') {
+        firstValue = '';
+        secondValue = '';
+        storedValue = '';
+        storedSecond = '';
+        calculator.dataset.previousKeyType = ''
+      } else button.textContent = 'AC';
+
+      display.textContent = 0;
+      calculator.dataset.previousKeyType = 'clear'
+    }
+
+    if(action === 'equals') {
+
+      firstValue = storedValue;
+      operator = lastOperator;
+      secondValue = display.textContent;
+      if(firstValue) {
+        if(calculator.dataset.previousKeyType === 'equals') {
+          firstValue = display.textContent;
+          secondValue = storedSecond;
+        }
+        display.textContent = operate(operator, firstValue, secondValue);
+      }
+      storedSecond = secondValue;
+      calculator.dataset.previousKeyType = 'equals'
+    }
   }
-})
+}
